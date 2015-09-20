732965d78651d2905fb6b5037c4b7c01f33ff513
Spencer Kaiser <skaiser@me.com>
2015-09-20 10:52:15 -0500

    Adding main script

---
+++ scripts/dinklebot.js
@@ -0,0 +1,46 @@
+/*global module,robot*/
+
+var scores = {};
+
+module.exports = function (robot) {
+    // SEND SCORING INFO
+    robot.hear(/.*score.*/i, function (msg) {
+        //Match any sentence containg score or scores
+        var scoreString = "Scoring still in development";
+        for (var item in scores) {
+            scoreString += item;
+        }
+
+        msg.reply(scoreString);
+    });
+
+    // ITEM++
+    robot.hear(/(.*)\+\+/i, function (msg) {
+        var item = msg.match[1];
+        if (scores.item) {
+            scores.item.score += 1;
+        } else {
+            scores.item = {};
+            scores.item.score = 1;
+        }
+        msg.reply(item + " has " + scores.item.score + " points.");
+    });
+    
+    // ITEM--
+    robot.hear(/(.*)--/i, function (msg) {
+        var item = msg.match[1];
+        if (scores.item) {
+            scores.item.score -= 1;
+        } else {
+            scores.item = {};
+            scores.item.score = -1;
+        }
+        msg.reply(item + " has " + scores.item.score + " points.");
+    });
+    
+
+    // GENERAL RESPONSE
+    robot.respond(/\?/i, function (msg) {
+        msg.reply("How may I be of assistance, Guardian?");
+    });
+};
