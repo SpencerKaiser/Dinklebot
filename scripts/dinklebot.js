de855d8435dbc3ce0920a2305b96712d4996ac55
Spencer Kaiser <skaiser@me.com>
2015-09-21 15:49:59 -0500

    Adding new function

--- scripts/dinklebot.js
+++ scripts/dinklebot.js
@@ -3,7 +3,7 @@ module.exports = function (robot) {
     var last = "";
     var lastToVote = "";
     //TODO
-    
+
     // *************** SCORING ***************
     // SEND SCORING INFO
     robot.hear(/.*score.*/i, function (msg) {
@@ -23,7 +23,7 @@ module.exports = function (robot) {
             sortedScores[scores[item].score].push(itemString);
         }
 
-        for (var i = sortedScores.length -1; i >=0; i--) {
+        for (var i = sortedScores.length - 1; i >= 0; i--) {
             if (sortedScores[i]) {
                 for (var j = 0; j < sortedScores[i].length; j++) {
                     scoreString += sortedScores[i][j];
@@ -58,12 +58,12 @@ module.exports = function (robot) {
         msg.send(item + " has " + scores[item].score + " points.");
     });
 
-    
+
     robot.hear(/^\+\+/i, function (msg) {
         scores[last].score += 1;
         msg.send(last + " has " + scores[last].score + " points.");
     });
-    
+
     robot.hear(/^\-\-/i, function (msg) {
         scores[last].score -= 1;
         msg.send(last + " has " + scores[last].score + " points.");
@@ -74,7 +74,7 @@ module.exports = function (robot) {
 
     // GENERAL RESPONSE
     robot.respond(/.?\?$/i, function (msg) {
-        msg.reply("How may I be of assistance, Guardian?");
+        msg.send("How may I be of assistance, Guardian?");
     });
 
     robot.hear(/wat/i, function (msg) {
@@ -84,16 +84,21 @@ module.exports = function (robot) {
     robot.hear(/just out for a rip are ya bud\?/i, function (msg) {
         msg.send("OOOH FUCK YEA BUD");
     });
-    
+
     robot.hear(/.*o+h fuck yea bud.*/i, function (msg) {
         msg.send("BRB, out for a rip...");
     });
-    
+
     robot.hear(/oh shit\!/i, function (msg) {
         msg.send("We've woken the Hive!");
     });
 
     robot.respond(/.*Who.?s the worst.*/i, function (msg) {
-        msg.reply("Slackbot, of course.");
+        msg.send("Slackbot, of course.");
+    });
+
+    robot.respond(/.*/i, function (msg) {
+        var quotes = ['It\'s in the walls!', 'Got \' em.', 'We\'ve woken the Hive!', 'I thought YOU had the hard job.', 'Well, at least he\'s chained up.', 'This path should lead us straight to the grave....... The World\'s Grave. Not ours.'];
+        msg.send(msg.random(quotes));
     });
 };
